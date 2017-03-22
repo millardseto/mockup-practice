c6682a0973ea7b4c535022701d3d70027d354467
Millard Seto <millardseto@mac.com>
2017-03-21 19:08:30 -0700

    After content loaded, wireup any additional a tags

--- script/main.js
+++ script/main.js
@@ -1,18 +1,27 @@
 $(function(){
 
-  /* load home page */
+  /* load home page - since user hasnt clicked anything yet */
   $("#main").load("main.html", function(response, status) {
     if (status != "success") {
       $("#main").text("failed to load page.");
     }
+
+    /* now that the partial page has loaded, wireup links on the partial page */
+    $('.specific-booking').click(function(){
+      event.preventDefault(); // for anchor tag, don't navigate
+      var file = this.href;
+      $("#main").load(file);
+    });
+
   });
 
-  $("nav a").click(function(){
+  /* click event handler for nav anchors and booking anchors */
+  $('nav a, .general-booking').click(function(){
     event.preventDefault(); // for anchor tag, don't navigate
     var file = this.href;
     $("#main").load(file, function(response, status){
-      if (status = "success"){
-
+      if (status == "success"){
+        /* if there are links in content pages, may need to wire up here */
       } else {
         $("#main").text = "failed to load page."
       }
